c3f16e2ae72f7b8ed23eaef92d5be073caa6d6b5
Dani <daniel.salmeron@gmail.com>
2024-10-13 23:02:36 +0200

    modifying openai variable name

--- lib/openai.ts
+++ lib/openai.ts
@@ -2,7 +2,7 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // Asegúrate de que esta variable esté definida
+  apiKey: process.env.OPENAI_API_KEY_DAN, // Asegúrate de que esta variable esté definida
 });
 
 export { openai };
